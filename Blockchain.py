6c6133779b9b9577246c5285e477bf891acda97c
Dylan <dyevki@gmail.com>
2025-02-04 19:19:10 +0000

    First Commit

---
+++ Blockchain.py
@@ -0,0 +1,30 @@
+#Module 1 - Create a BlockChain
+
+
+#Importing Libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#Part 1 - Building a blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        
+        self.chain.append(block)
+        return block
+    
+    def get_prev_block(self): 
+        return self.chain[-1]
+    
